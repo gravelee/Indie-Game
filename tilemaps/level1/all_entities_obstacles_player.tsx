c9061e65d91ec82a3fddd4ea8dff068a9abf0b33
gravelee <86350945+gravelee@users.noreply.github.com>
2026-04-10 16:40:25 +0300

    Added the rest of the files.
    Last commit.

---
+++ tilemaps/level1/all_entities_obstacles_player.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="all_entities_obstacles_player" tilewidth="96" tileheight="96" tilecount="200" columns="200">
+ <tileoffset x="-32" y="32"/>
+ <image source="../all_entities_obstacles_player.png" width="19200" height="96"/>
+ <tile id="1">
+  <properties>
+   <property name="Drawning Offset" type="int" value="-50"/>
+  </properties>
+ </tile>
+</tileset>
